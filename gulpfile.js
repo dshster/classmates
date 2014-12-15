318f018704b8f384d52a95e6efacdaebab632e98
Dmitry Shvalyov <dsh-creative+subscription@yandex.ru>
2014-12-15 22:32:21 +0300

    Gulp bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+/* global require, process, console */
+'use strict';
+
+var gulp = require('gulp'),
+    path = require('path'),
+    prefs = require('./preferences.json'),
+    $ = require('gulp-load-plugins')();
+
+
+gulp.task('template', function(next) {
+});
+
+gulp.task('styles', function(next) {
+});
+
+gulp.task('server', function(next) {
+});
+
+gulp.task('default');
